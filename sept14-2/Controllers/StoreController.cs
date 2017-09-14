757d2da77df399f56f90cf53f63bfb408f28abbd
Mykyta <nesspire00@gmail.com>
2017-09-14 13:35:20 -0400

    Woo 2

---
+++ sept14-2/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sept14_2.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            ViewBag.Genre = genre;
+            return View();
+        }
+    }
+}
